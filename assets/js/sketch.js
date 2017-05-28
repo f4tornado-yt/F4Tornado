131e438d04ae5761ff60f95c5e033efe91b5fb35
F4Tornado <h.rovnyak@gmail.com>
2017-05-28 10:59:39 -0400

    m4as to mp3s

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -7,7 +7,7 @@ var spaceinvasionMenu;
 function preload() {
     airplanes = loadSound('assets/sounds/airplanes.mp3');
     bells = loadSound('assets/sounds/bells and crackle.mp3');
-    ghostbees = loadSound('assets/sounds/ghost bees.m4a');
+    ghostbees = loadSound('assets/sounds/ghost bees.mp3');
     sapphire = loadSound('assets/sounds/Sapphire Clouds.mp3');
     spaceinvasion = loadSound('assets/sounds/space invasion.mp3');
 }
@@ -24,7 +24,7 @@ function setup() {
     text('Space Invasion', 30, 310);
     airplanesMenu = new soundMenu(airplanes, 30, 40, 'assets/sounds/airplanes.mp3');
     bellsMenu = new soundMenu(bells, 30, 110, 'assets/sounds/bells and crackle.mp3');
-    ghostbeesMenu = new soundMenu(ghostbees, 30, 180, 'assets/sounds/ghost bees.m4a');
+    ghostbeesMenu = new soundMenu(ghostbees, 30, 180, 'assets/sounds/ghost bees.mp3');
     sapphireMenu = new soundMenu(sapphire, 30, 260, 'assets/sounds/Sapphire Clouds.mp3');
     spaceinvasionMenu = new soundMenu(spaceinvasion, 30, 340, 'assets/sounds/space invasion.mp3');
 }
